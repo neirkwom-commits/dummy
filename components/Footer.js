1ad31da897074dbe1b1e7aaf44ceb359a755c3f7
neirkwom-commits <neirkwom@gmail.com>
2025-08-10 05:59:14 +0700

    Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#001f27',
+      color: '#00ffd5',
+      padding: '20px 0',
+      textAlign: 'center',
+      fontWeight: '700',
+      marginTop: '40px',
+    }}>
+      &copy; 2025 DSRT - Digital Smart Revise Technology. All rights reserved.
+    </footer>
+  );
+}
